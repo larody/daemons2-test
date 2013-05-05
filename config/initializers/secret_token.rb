d99d75618acd47eeec0efcb8151d2d467ed4dfdd
larody <throwbases@gmail.com>
2013-05-05 20:34:28 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daemons2Test::Application.config.secret_token = '1662d28f4a5c8fb2bcffeab803f88254a06a3d87886304ca42eb138de3e956189ef75b424bf587afc90cfe5c4f48b5798d915dec834a80fa017475857d64b9f7'
